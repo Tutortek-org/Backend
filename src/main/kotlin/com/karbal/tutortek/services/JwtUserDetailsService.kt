3da5080b0a1b55412080f753a9cfc5bdf261317c
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-10-03 20:20:26 +0300

    Create JwtUserDetailsService.kt

---
+++ src/main/kotlin/com/karbal/tutortek/services/JwtUserDetailsService.kt
@@ -0,0 +1,16 @@
+package com.karbal.tutortek.services
+
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class JwtUserDetailsService : UserDetailsService {
+    override fun loadUserByUsername(username: String?): UserDetails {
+        if(username == "javanuise")
+            return User("javanuise", "\$2a\$10\$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", arrayListOf())
+        else throw UsernameNotFoundException("User not found with username: $username")
+    }
+}
