0585305536a39a2acbef9529493e6b40907b583a
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-10-03 21:28:42 +0300

    Implement JwtRequestFilter

---
+++ src/main/kotlin/com/karbal/tutortek/security/JwtRequestFilter.kt
@@ -0,0 +1,41 @@
+package com.karbal.tutortek.security
+
+import com.karbal.tutortek.services.JwtUserDetailsService
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+
+@Component
+class JwtRequestFilter(
+    val jwtUserDetailsService: JwtUserDetailsService,
+    val jwtTokenUtil: JwtTokenUtil
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+        val requestTokenHeader = request.getHeader("Authorization")
+
+        var username: String? = null
+        var jwtToken: String = ""
+
+        if(requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7)
+            username = jwtTokenUtil.getUsernameFromToken(jwtToken)
+        }
+
+        if(username != null && SecurityContextHolder.getContext().authentication == null) {
+            val userDetails = jwtUserDetailsService.loadUserByUsername(username)
+
+            if(jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+                val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
+                usernamePasswordAuthenticationToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+                SecurityContextHolder.getContext().authentication = usernamePasswordAuthenticationToken
+            }
+        }
+        chain.doFilter(request, response)
+    }
+}
