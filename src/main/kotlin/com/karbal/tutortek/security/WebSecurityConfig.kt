b08652f08062b402a9890754009981f4a4f5a954
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-10-03 21:55:57 +0300

    Create WebSecurityConfig.kt

---
+++ src/main/kotlin/com/karbal/tutortek/security/WebSecurityConfig.kt
@@ -0,0 +1,42 @@
+package com.karbal.tutortek.security
+
+import com.karbal.tutortek.services.JwtUserDetailsService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+class WebSecurityConfig(
+    private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
+    private val jwtUserDetailsService: JwtUserDetailsService,
+    private val jwtRequestFilter: JwtRequestFilter
+) : WebSecurityConfigurerAdapter() {
+
+    @Autowired
+    fun configureGlobal(authenticationManagerBuilder: AuthenticationManagerBuilder) {
+        authenticationManagerBuilder.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder())
+    }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+
+    override fun configure(http: HttpSecurity?) {
+        http?.csrf()?.disable()
+            ?.authorizeRequests()?.antMatchers("/authenticate")?.permitAll()
+            ?.anyRequest()?.authenticated()?.and()
+            ?.exceptionHandling()?.authenticationEntryPoint(jwtAuthenticationEntryPoint)?.and()
+            ?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        http?.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter::class.java)
+    }
+}
