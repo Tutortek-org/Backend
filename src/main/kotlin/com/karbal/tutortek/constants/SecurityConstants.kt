f6a00dc40179f6f2079c7b3c96f6e2ec3978817a
Zablas <vagabaliukas@gmail.com>
2021-10-05 21:48:25 +0300

    Add refresh token handling

--- src/main/kotlin/com/karbal/tutortek/constants/SecurityConstants.kt
+++ src/main/kotlin/com/karbal/tutortek/constants/SecurityConstants.kt
@@ -3,10 +3,12 @@ package com.karbal.tutortek.constants
 class SecurityConstants {
     companion object {
         const val AUTHORIZATION_HEADER = "Authorization"
+        const val REFRESH_TOKEN_HEADER = "isRefreshToken"
         const val TOKEN_BEGINNING = "Bearer "
         const val TOKEN_EXPIRATION = 18_000_000
         const val REFRESH_EXPIRATION = 9_000_000
         const val LOGIN_ENDPOINT = "/login"
         const val REGISTER_ENDPOINT = "/register"
+        const val REFRESH_ENDPOINT = "/refresh"
     }
 }
--- src/main/kotlin/com/karbal/tutortek/security/JwtRequestFilter.kt
+++ src/main/kotlin/com/karbal/tutortek/security/JwtRequestFilter.kt
@@ -2,6 +2,7 @@ package com.karbal.tutortek.security
 
 import com.karbal.tutortek.constants.SecurityConstants
 import com.karbal.tutortek.services.JwtUserDetailsService
+import io.jsonwebtoken.ExpiredJwtException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
@@ -18,25 +19,45 @@ class JwtRequestFilter(
 ) : OncePerRequestFilter() {
 
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
-        val requestTokenHeader = request.getHeader(SecurityConstants.AUTHORIZATION_HEADER)
+        try {
+            val requestTokenHeader = request.getHeader(SecurityConstants.AUTHORIZATION_HEADER)
+            var username: String? = null
+            var jwtToken = ""
 
-        var username: String? = null
-        var jwtToken = ""
-
-        if(requestTokenHeader != null && requestTokenHeader.startsWith(SecurityConstants.TOKEN_BEGINNING)) {
-            jwtToken = requestTokenHeader.substring(SecurityConstants.TOKEN_BEGINNING.length)
-            username = jwtTokenUtil.getUsernameFromToken(jwtToken)
-        }
-
-        if(username != null && SecurityContextHolder.getContext().authentication == null) {
-            val userDetails = jwtUserDetailsService.loadUserByUsername(username)
-
-            if(jwtTokenUtil.validateToken(jwtToken, userDetails)) {
-                val usernamePasswordAuthenticationToken = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
-                usernamePasswordAuthenticationToken.details = WebAuthenticationDetailsSource().buildDetails(request)
-                SecurityContextHolder.getContext().authentication = usernamePasswordAuthenticationToken
+            if(requestTokenHeader != null && requestTokenHeader.startsWith(SecurityConstants.TOKEN_BEGINNING)) {
+                jwtToken = requestTokenHeader.substring(SecurityConstants.TOKEN_BEGINNING.length)
+                username = jwtTokenUtil.getUsernameFromToken(jwtToken)
             }
+
+            if(username != null && SecurityContextHolder.getContext().authentication == null)
+                setAuthenticationTokenOnSecurityContext(username, jwtToken, request)
+        }
+        catch (e: ExpiredJwtException) {
+            handleExpiredJwt(request, e)
         }
         chain.doFilter(request, response)
+    }
+
+    private fun handleExpiredJwt(request: HttpServletRequest, e: ExpiredJwtException) {
+        val isRefreshToken = request.getHeader(SecurityConstants.REFRESH_TOKEN_HEADER)
+        val requestUrl = request.requestURL.toString()
+        if (isRefreshToken == "true" && requestUrl.contains(SecurityConstants.REFRESH_ENDPOINT))
+            allowForRefreshToken(e, request)
+        else request.setAttribute("exception", e)
+    }
+
+    private fun allowForRefreshToken(exception: ExpiredJwtException, request: HttpServletRequest) {
+        SecurityContextHolder.getContext().authentication = UsernamePasswordAuthenticationToken(null, null, null)
+        request.setAttribute("claims", exception.claims)
+    }
+
+    private fun setAuthenticationTokenOnSecurityContext(username: String?, jwtToken: String, request: HttpServletRequest) {
+        val userDetails = jwtUserDetailsService.loadUserByUsername(username)
+        if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+            val usernamePasswordAuthenticationToken =
+                UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
+            usernamePasswordAuthenticationToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+            SecurityContextHolder.getContext().authentication = usernamePasswordAuthenticationToken
+        }
     }
 }
