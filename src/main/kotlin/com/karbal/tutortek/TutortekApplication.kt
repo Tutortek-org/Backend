e043417b98b8e6fde2eda276cbfcc239ee956904
Zablas <vagabaliukas@gmail.com>
2022-02-16 19:24:22 +0200

    Update TutortekApplication.kt

--- src/main/kotlin/com/karbal/tutortek/TutortekApplication.kt
+++ src/main/kotlin/com/karbal/tutortek/TutortekApplication.kt
@@ -4,11 +4,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class TutortekApplication {
 	@Bean
 	fun passwordEncoder() = BCryptPasswordEncoder()
+
+	@Bean
+	fun corsConfigurer() = object : WebMvcConfigurer{
+		override fun addCorsMappings(registry: CorsRegistry) {
+			registry.addMapping("*").allowedOrigins("*")
+		}
+	}
 }
 
 fun main(args: Array<String>) {
