51f646dca5f459f0415e9fd56eb5541508781bb3
Zablas <vagabaliukas@gmail.com>
2022-03-10 14:29:04 +0200

    Implement needed services for bug reports

--- src/main/kotlin/com/karbal/tutortek/controllers/BugReportController.kt
+++ src/main/kotlin/com/karbal/tutortek/controllers/BugReportController.kt
@@ -1,10 +1,45 @@
 package com.karbal.tutortek.controllers
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.karbal.tutortek.constants.ApiErrorSlug
+import com.karbal.tutortek.dto.bugReportDTO.BugReportGetDTO
+import com.karbal.tutortek.dto.bugReportDTO.BugReportPostDTO
+import com.karbal.tutortek.entities.BugReport
+import com.karbal.tutortek.security.Role
+import com.karbal.tutortek.services.BugReportService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
+import javax.annotation.security.RolesAllowed
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("bugreports")
-class BugReportController {
+class BugReportController(val bugReportService: BugReportService) {
 
+    @GetMapping
+    @RolesAllowed(Role.ADMIN_ANNOTATION)
+    fun getAllBugReports() = bugReportService.getAllBugReports().map { br -> BugReportGetDTO(br) }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createBugReport(@RequestBody bugReportPostDTO: BugReportPostDTO, request: HttpServletRequest) {
+        verifyDto(bugReportPostDTO)
+
+    }
+
+    private fun convertDtoToEntity(bugReportPostDTO: BugReportPostDTO, request: HttpServletRequest): BugReport {
+        val bugReport = BugReport()
+
+
+
+        return bugReport
+    }
+
+    private fun verifyDto(bugReportPostDTO: BugReportPostDTO) {
+        if(bugReportPostDTO.name.isEmpty())
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, ApiErrorSlug.NAME_EMPTY)
+
+        if(bugReportPostDTO.description.isEmpty())
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, ApiErrorSlug.DESCRIPTION_EMPTY)
+    }
 }
---
+++ src/main/kotlin/com/karbal/tutortek/services/BugReportService.kt
@@ -0,0 +1,15 @@
+package com.karbal.tutortek.services
+
+import com.karbal.tutortek.entities.BugReport
+import com.karbal.tutortek.repositories.BugReportRepository
+import org.springframework.stereotype.Service
+
+@Service
+class BugReportService(val database: BugReportRepository) {
+
+    fun getAllBugReports(): List<BugReport> = database.getAllBugReports()
+
+    fun saveBugReport(bugReport: BugReport) = database.save(bugReport)
+
+    fun deleteBugReport(id: Long) = database.deleteById(id)
+}
