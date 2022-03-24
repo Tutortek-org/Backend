e89a55c711b9b5e9099b859fbdf05c4b7232a243
Zablas <vagabaliukas@gmail.com>
2022-03-24 14:16:08 +0200

    Add user profile ID to topic get DTO

--- src/main/kotlin/com/karbal/tutortek/dto/topicDTO/TopicGetDTO.kt
+++ src/main/kotlin/com/karbal/tutortek/dto/topicDTO/TopicGetDTO.kt
@@ -5,11 +5,13 @@ import com.karbal.tutortek.entities.Topic
 data class TopicGetDTO(
     val id: Long?,
     val name: String,
-    val isApproved: Boolean
+    val isApproved: Boolean,
+    val userId: Long?
 ){
     constructor(topic: Topic) : this(
         topic.id,
         topic.name,
-        topic.isApproved
+        topic.isApproved,
+        topic.userProfile.id
     )
 }
