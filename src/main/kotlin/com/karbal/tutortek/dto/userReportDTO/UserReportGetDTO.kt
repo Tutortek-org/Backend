22a93febab96b5e5ba53cc89ffbf71084fbd9318
Zablas <vagabaliukas@gmail.com>
2022-04-01 16:43:43 +0300

    Update UserReportGetDTO.kt

--- src/main/kotlin/com/karbal/tutortek/dto/userReportDTO/UserReportGetDTO.kt
+++ src/main/kotlin/com/karbal/tutortek/dto/userReportDTO/UserReportGetDTO.kt
@@ -1,13 +1,18 @@
 package com.karbal.tutortek.dto.userReportDTO
 
+import com.karbal.tutortek.dto.userDTO.UserGetDTO
 import com.karbal.tutortek.entities.UserReport
 
 data class UserReportGetDTO(
     val id: Long?,
-    val description: String
+    val description: String,
+    val reported: UserGetDTO,
+    val reporter: UserGetDTO
 ) {
     constructor(userReport: UserReport) : this(
         userReport.id,
-        userReport.description
+        userReport.description,
+        UserGetDTO(userReport.reportOf),
+        UserGetDTO(userReport.reportedBy)
     )
 }
