e89f925f621e1b6caebd7b9cc920d005bcdcee8d
Zablas <vagabaliukas@gmail.com>
2022-05-22 23:49:31 +0300

    Update NotificationController.kt

--- src/main/kotlin/com/karbal/tutortek/controllers/NotificationController.kt
+++ src/main/kotlin/com/karbal/tutortek/controllers/NotificationController.kt
@@ -11,7 +11,7 @@ import com.karbal.tutortek.services.UserProfileService
 import com.karbal.tutortek.utils.SNSUtils
 import io.jsonwebtoken.impl.DefaultClaims
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -55,8 +55,7 @@ class NotificationController(
         SNSUtils.sendNotifications(notificationPostDTO)
     }
 
-    @DeleteMapping("cleararn")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("cleararn")
     fun clearArn(request: HttpServletRequest) {
         var claims = request.getAttribute(SecurityConstants.CLAIMS_ATTRIBUTE) as DefaultClaims?
         if(claims == null) claims = jwtTokenUtil.parseClaimsFromRequest(request)
